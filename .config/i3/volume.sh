4b181142a1c52b4975ae268d4c9fce9b9169fc42
Clem0908 <clementlyonnet@hotmail.fr>
2024-01-08 13:13:18 +0100

    update

--- .config/i3/volume.sh
+++ .config/i3/volume.sh
@@ -1,6 +1,4 @@
 #!/bin/zsh
 
-msgTag="Volume : "
-
 volume=$(pactl get-sink-volume 0 | grep -e "[0-9][0-9]%" -e "[0-9]%" -o | tail -1)
-notify-send -u low -t 1000 $msgTag$volume
+notify-send -a Volume -u low -t 1000 $volume
