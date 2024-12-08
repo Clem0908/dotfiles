a337449facdd7015ed43a3c8797596fa6e8bc6be
Clem0908 <clementlyonnet@hotmail.fr>
2024-12-08 15:00:26 +0100

    update

--- .config/sway/status.sh
+++ .config/sway/status.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-date=$(date '+%A %d %B %Y %T')
-battery_cap=$(cat /sys/class/power_supply/BAT0/capacity)
-battery_st=$(cat /sys/class/power_supply/BAT0/status)
+date=$(date '+%d/%m/%Y %T')
+load=$(uptime | cut -c 46-49)
+cpu=$(cat /proc/cpuinfo | grep -e "cpu MHz" | cut -c 12-15)
+#battery_cap=$(cat /sys/class/power_supply/BAT0/capacity)
+#battery_st=$(cat /sys/class/power_supply/BAT0/status)
 
-echo " $date | $battery_st | $battery_cap% "
+echo "$cpu MHz | Load: $load | $date"
