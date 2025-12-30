52ad925731d5d00c9292f3235c1ae52b2e70c579
Clem0908 <clementlyonnet@hotmail.fr>
2025-12-30 22:25:08 +0100

    updated sway configs

--- sway/status.sh
+++ sway/status.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 date=$(date '+%d/%m/%Y %T')
-load=$(uptime | cut -c 46-49)
-cpu=$(cat /proc/cpuinfo | grep -e "cpu MHz" | cut -c 12-15)
+cpu=$(cat /proc/cpuinfo | grep -e "cpu MHz" -m 1 | cut -c 12-15)
+cpu_temp=$(cat /dev/cpu_temp | cut -c 1-2)
+gpu_temp=$(cat /dev/gpu_temp | cut -c 1-2)
+ram=$(grep MemAvailable /proc/meminfo | cut -c 17-18)
 #battery_cap=$(cat /sys/class/power_supply/BAT0/capacity)
 #battery_st=$(cat /sys/class/power_supply/BAT0/status)
 
-echo "$cpu MHz | Load: $load | $date"
+echo " $cpu MHz |  $cpu_temp°C |  $gpu_temp°C |  $ram Go | $date"
